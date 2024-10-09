c63bf20868049b3445579908c76b5f282987e3fd
Victoria Gallo <164115980+VictoriaGallo@users.noreply.github.com>
2024-10-09 15:01:22 -0500

    Add files via upload

---
+++ GiftExpertApp.js
@@ -0,0 +1,23 @@
+
+import { useState } from 'react';
+import { AddCategory, GifGrid } from './components';
+
+export const GifExpertApp = () => {
+    const [categories, setCategories] = useState(['Zodiac']);
+
+    const onAddCategory = (category) => {
+        setCategories(list => [...list, category]);
+    }
+
+    return (
+        <>
+            <h1>GifExpert</h1>
+            <AddCategory onAddCategory={onAddCategory} />
+            {
+                categories.map(category => (
+                    <GifGrid key={category} category={category} />
+                ))
+            }
+        </>
+    );
+}
