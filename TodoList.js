1cecd39eb26f43a428e1b4d813d158d5646dc1d8
Victoria Gallo <164115980+VictoriaGallo@users.noreply.github.com>
2024-11-04 22:09:28 -0500

    Create TodoList.js

---
+++ TodoList.js
@@ -0,0 +1,22 @@
+const TodoList = ({ todos, handleDelete }) => {
+    return (
+      <div className="col-7">
+        <ul className="list-group">
+          {todos.map((todo) => (
+            <li key={todo.id} className="list-group-item d-flex justify-content-between">
+              <span className="align-self-center">{todo.description}</span>
+              <button 
+                className="btn btn-danger" 
+                onClick={() => handleDelete(todo.id)}
+              >
+                Borrar
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  };
+  
+  export default TodoList;
+  
