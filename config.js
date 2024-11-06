d6c94455a8775cc0f29a580724a5ab9f4932d8b7
Victoria Gallo <164115980+VictoriaGallo@users.noreply.github.com>
2024-11-06 00:18:30 -0500

    Update config.js

--- config.js
+++ config.js
@@ -3,12 +3,12 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAqqN_Ulb_9-o47w20u-5AMl8mB6qIGHfg",
+  apiKey: " ",
   authDomain: "edya2-challenges.firebaseapp.com",
   projectId: "edya2-challenges",
   storageBucket: "edya2-challenges.appspot.com",
   messagingSenderId: "1020739286342",
-  appId: "1:1020739286342:web:b4d746fda6d79215776147"
+  appId: " "
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
